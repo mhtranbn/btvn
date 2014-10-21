a1925efb0b39c23ba84c8dd87acb732c8686d3dc
mhtran <mhtran@LongAnPhus-MacBook-Pro.local>
2014-10-22 03:07:30 +0700

    DemoPanGravity

---
+++ DynamicsKitDemo/DynamicsKitDemo/DemoPanGravity.swift
@@ -0,0 +1,50 @@
+//
+//  DemoPanGravity.swift
+//  DynamicsKitDemo
+//
+//  Created by mhtran on 10/17/14.
+//  Copyright (c) 2014 vn.Techmsater. All rights reserved.
+//
+
+import UIKit
+
+class DemoPanGravity: UIViewController {
+    var bi: UIImageView?
+    var animator: UIDynamicAnimator?
+    var gravity = UIGravityBehavior()
+    var collider = UICollisionBehavior()
+    override func loadView() {
+        super.loadView()
+        var size = self.view.bounds.size
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.edgesForExtendedLayout = UIRectEdge.None
+        
+        bi = UIImageView(frame: CGRect(x: size.width * 0.5, y: 0, width: 60, height: 60))
+        bi?.image = UIImage(named: "bi0.png")
+        let tap = UITapGestureRecognizer(target: self, action: "onTap:")
+        self.view.addGestureRecognizer(tap)
+    }
+    func configureDynamicKit() {
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity.gravityDirection = CGVectorMake(0.0, 0.8)
+        animator!.addBehavior(gravity)
+        collider.translatesReferenceBoundsIntoBoundary = true
+        animator!.addBehavior(collider)
+    }
+    func addBoxToPhysic() {
+        configureDynamicKit()
+        collider.addItem(bi!)
+        gravity.addItem(bi!)
+    }
+    func onTap(tap: UIGestureRecognizer) {
+        let point = tap.locationInView(self.view)
+        self.bi?.center = CGPoint(x: point.x, y: point.y)
+        self.view.addSubview(bi!)
+        NSLog("cham bi")
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(),{
+            self.addBoxToPhysic()
+            NSLog("Roibi")
+            })
+//        self.addBoxToPhysic()
+    }
+}
